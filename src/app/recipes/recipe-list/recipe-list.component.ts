5044674225910153bcf681be2344a2ed76d2d7ea
Paaa <p.m.prudnikov@gmail.com>
2016-10-31 22:53:08 +0300

    Recipes and  recipe list components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent{
+
+  constructor() { }
+
+}
